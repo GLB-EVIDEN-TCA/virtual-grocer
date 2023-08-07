a4ae02ab4691422c2cf3e3c579e07b4f42aeb0e1
Jack DeStefano <jack.destefano@QX4XC35HYG-Maven-Wave.local>
2023-08-07 15:49:44 -0500

    creating new branch with broken code for adding a client authentication service

--- src/Eviden.VirtualGrocer/Client/Program.cs
+++ src/Eviden.VirtualGrocer/Client/Program.cs
@@ -1,11 +1,34 @@
 using Eviden.VirtualGrocer.Web.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.Extensions.DependencyInjection
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+public class BaseAddressAuthorizationMessageHandler :
+Microsoft.AspNetCore.Components.WebAssembly.Authentication.AuthorizationMessageHandler
+
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
+builder.Services.AddHttpClient("Eviden.VirtualGrocer.Web.Client.ServerAPI", client => 
+        client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress))
+    .AddHttpMessageHandler<BaseAddressAuthorizationMessageHandler>();
+
+builder.Services.AddScoped(sp => sp.GetRequiredService<IHttpClientFactory>()
+    .CreateClient("Eviden.VirtualGrocer.Web.Client.ServerAPI"));
 await builder.Build().RunAsync();
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("{SCOPE URI}");
+});
+//add DI bin to client side
+//builder.Services.AddSingleton<IConfiguration>(config);
+//builder.Services.AddAzureSearch(azureSearchEndpoint!, azureSearchIndex!, azureSearchKey!);
+//builder.Services.AddAzureChatCompletion(azureAiEndpoint!, azureAiModel!, azureAiKey!);
+
