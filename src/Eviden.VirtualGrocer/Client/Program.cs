e4847aaf810005df8b0fc2163ee8c41b00f42d01
Dave Arlin <davearlin@gmail.com>
2023-08-07 16:03:53 -0500

    Added missing packages and cleaned up some syntax.

--- src/Eviden.VirtualGrocer/Client/Program.cs
+++ src/Eviden.VirtualGrocer/Client/Program.cs
@@ -2,16 +2,11 @@ using Eviden.VirtualGrocer.Web.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
-using Microsoft.Extensions.DependencyInjection
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
-public class BaseAddressAuthorizationMessageHandler :
-Microsoft.AspNetCore.Components.WebAssembly.Authentication.AuthorizationMessageHandler
-
-
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddHttpClient("Eviden.VirtualGrocer.Web.Client.ServerAPI", client => 
