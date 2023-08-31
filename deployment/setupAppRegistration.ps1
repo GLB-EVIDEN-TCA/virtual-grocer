aa6f1f546cb27305952fe193d16b40eee6c16fc9
Jeremy Keller <jeremy.keller-ext@jci.com>
2023-08-31 08:47:18 -0700

    Adding powershell script for App Registration to Bicep template

---
+++ deployment/setupAppRegistration.ps1
@@ -0,0 +1,18 @@
+﻿param([string] $webAppName)
+
+Connect-AzAccount -Identity
+
+# Create app registration
+$app = New-AzADApplication -DisplayName $webAppName -SigninAudience AzureADandPersonalMicrosoftAccount
+
+# Set domain name and URLs
+$DOMAIN_NAME = "$webAppName.azurewebsites.net"
+$REPLY_URL_1 = "https://$DOMAIN_NAME/authentication/login-callback"
+$REPLY_URL_1 = "https://$DOMAIN_NAME/authentication/login-callback"
+$LOGOUT_URL = "https://$DOMAIN_NAME/signout-oidc"
+
+# Update the application
+Update-AzADApplication -ObjectId $CLIENT_ID -ReplyUrls $REPLY_URL_1 -LogoutUrl $LOGOUT_URL -Oauth2AllowImplicitFlow $true
+
+$DeploymentScriptOutputs = @{}
+$DeploymentScriptOutputs['clientId'] = $app.appId
